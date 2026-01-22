3a87484eb8107be66d7243c1bcaf2d0d0da99678
Areeba Izhar <areebaizhar2295@gmail.com>
2026-01-22 11:27:29 +0500

    Changes to Task 1 a

--- Lab02/Task1a.s
+++ Lab02/Task1a.s
@@ -2,6 +2,12 @@
 .text
 .globl main
 main:
+    li x22, 10          # i = 0
+    li x23, 10         # j = 10
+    li x20, 5          # g = 5
+    li x21, 3          # h = 3
+    li x19, 0          # f = 0
+Loop:
     bne x22, x23, Else # if (i==j): f= g+h  | Else f= g-h
     add x19, x20, x21
     beq x0, x0, Exit
@@ -10,7 +16,15 @@ Else:
     sub x19, x20, x21
 Exit:
     j end
+
+end:
+    j end
     
 # exit and end are outside the loop bz they are the steps that define 
 #what the count is to cause the jump. 
 #main has the f else condition only
+
+# ctrl+shift + P: assemble and run 
+# in assember take the middle hex value, convert to binary, use the table in RISC=V greencard reference to distrubite the bits. 
+# cross check values for the imm[12|10:5] and imm[4:1|11] with the binary value to get the value for the distance.
+
