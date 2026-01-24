db35d8805a7bb8224b777230a26792eac0157de5
Areeba Izhar <areebaizhar2295@gmail.com>
2026-01-24 13:12:50 +0500

    comments task 2

--- Lab02/Task2.s
+++ Lab02/Task2.s
@@ -1,17 +1,17 @@
 .text
 .globl main
 main:
-    # --- 1. Assign Suitable Values First ---
+    #valyes for a b c and x
     li x21, 0 #a
     li x22, 20          # b = 20
     li x23, 10          # c = 10
-    li x20, 4           # x = 1
+    li x20, 4           # x = 1 or 2 or 3 or 4
 
-    li x6, 1
-    li x7, 2
-    li x28, 3
-    li x29, 4
-    li x5, 0
+    li x6, 1 #if x ==1
+    li x7, 2 #if x==2
+    li x28, 3 #if x==3
+    li x29, 4 #if x==4
+    li x5, 0 #default case 
     li x30, 2 # temp var for division and multi
 
     beq x20, x6, Case1
